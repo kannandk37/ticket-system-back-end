320f8b2a17ae566ea911cc6dc17bd69e7eb17f21
Kannan <kannandk37@gmail.com>
2025-09-25 04:06:40 +0530

    adds env

--- src/connection/index.ts
+++ src/connection/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri = `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE_NAME}?authSource=admin`;
+const uri = process.env.DB_URI ? process.env.DB_URI : `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE_NAME}?authSource=admin`;
 
 export function connectToDatabase() {
     try {
