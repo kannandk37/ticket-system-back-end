5e8853b39bb3569b54cb601b27d25383f3ff4dcd
Kannan <114098167+kannandk37@users.noreply.github.com>
2025-09-25 20:08:59 +0530

    Update seed-tickets.ts

--- src/seed/seed-tickets.ts
+++ src/seed/seed-tickets.ts
@@ -12,10 +12,10 @@ export async function importTickets() {
     return new Promise<Ticket[]>(async (resolve, reject) => {
         try {
             await connectToDatabase();
-            let existingTickets = await new TicketBusinessManager().tickets(null, null, null, null);
-            if (existingTickets?.length != 0) {
-                return resolve(existingTickets)
-            } else {
+            // let existingTickets = await new TicketBusinessManager().tickets(null, null, null, null);
+            // if (existingTickets?.length != 0) {
+            //     return resolve(existingTickets)
+            // } else {
                 console.log(`Seeding tickets, in process`);
 
                 const count = Number(process.env.TICKET_SEED_COUNT);
@@ -27,16 +27,16 @@ export async function importTickets() {
                     priority: faker.helpers.arrayElement([TicketPriority.LOW, TicketPriority.MEDIUM, TicketPriority.HIGH] as const),
                 }));
 
-                let tickets = await TicketSchema.insertMany(ticketEntitiesToTicketRecords(ticketSeedData));
+            //     let tickets = await TicketSchema.insertMany(ticketEntitiesToTicketRecords(ticketSeedData));
                 console.log(`Seeded ${ticketSeedData.length} tickets`);
-                if (tickets?.length == count) {
-                    let ticketsData = ticketRecordsToTicketEntities(tickets);
+            //     if (tickets?.length == count) {
+            //         let ticketsData = ticketRecordsToTicketEntities(tickets);
                     console.log(`Seeding tickets, ended`);
-                    return resolve(ticketsData)
-                } else {
-                    return reject("Unable to Seed Ticket for the given count");
-                }
-            }
+                    return resolve(ticketSeedData)
+            //     } else {
+            //         return reject("Unable to Seed Ticket for the given count");
+            //     }
+            // }
         } catch (error) {
             reject(error);
         }
